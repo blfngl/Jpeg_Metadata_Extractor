fa926cfb639c4122c4e6280270c7907d546d34f2
Nick Mirasol <takutastrong@yahoo.com>
2019-09-26 08:45:01 -0700

    Created main class

---
+++ src/main/java/mirasoln/jme/Main.java
@@ -0,0 +1,14 @@
+
+/**
+ * JME - Jpeg Metadata Extractor
+ * @author Nick Mirasol
+ * @date 9/26/2019
+ * @description A tool to extract metadata from a jpeg or set of jpegs
+ */
+
+package mirasoln.jme;
+
+public class Main
+{
+	
+}
