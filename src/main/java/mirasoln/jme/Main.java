f281f54d4c2db1721deb22cab44617e2488df339
Nick Mirasol <takutastrong@yahoo.com>
2019-09-26 09:33:45 -0700

    Added gui menu + zipcode display

--- src/main/java/mirasoln/jme/Main.java
+++ src/main/java/mirasoln/jme/Main.java
@@ -8,7 +8,14 @@
 
 package mirasoln.jme;
 
+import java.awt.BorderLayout;
+
 import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JMenu;
+import javax.swing.JMenuBar;
+import javax.swing.JMenuItem;
+import javax.swing.JPanel;
 
 public class Main
 {
@@ -18,6 +25,31 @@ public class Main
 		JFrame frame = new JFrame("JME - JPEG Metadata Extractor");
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.setSize(600, 600);
+
+		// Menu bar
+		JMenuBar mb = new JMenuBar();
+		JMenu menuFile = new JMenu("File");
+		JMenu menuOptions = new JMenu("Options");
+		JMenu menuHelp = new JMenu("Help");
+
+		mb.add(menuFile);
+		mb.add(menuOptions);
+		mb.add(menuHelp);
+
+		JMenuItem menuFile_Open = new JMenuItem("Open");
+		JMenuItem menuFile_Dump = new JMenuItem("Dump metadata");
+
+		menuFile.add(menuFile_Open);
+		menuFile.add(menuFile_Dump);
+
+		frame.getContentPane().add(BorderLayout.NORTH, mb);
+
+		// ZIP code display
+		JPanel panelZip = new JPanel();
+		JLabel labelPanelZip = new JLabel("ZIP code:");
+		panelZip.add(labelPanelZip);
+
+		frame.getContentPane().add(BorderLayout.SOUTH, panelZip);
 
 		System.out.println("Opening frame");
 		frame.setVisible(true);
