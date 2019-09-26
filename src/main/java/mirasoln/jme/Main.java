b9d6e1f5981b11fff0cb155344793592e47f00b4
Nick Mirasol <takutastrong@yahoo.com>
2019-09-26 09:19:32 -0700

    Created basic window

--- src/main/java/mirasoln/jme/Main.java
+++ src/main/java/mirasoln/jme/Main.java
@@ -1,6 +1,6 @@
 
 /**
- * JME - Jpeg Metadata Extractor
+ * JME - JPEG Metadata Extractor
  * @author Nick Mirasol
  * @date 9/26/2019
  * @description A tool to extract metadata from a jpeg or set of jpegs
@@ -8,7 +8,18 @@
 
 package mirasoln.jme;
 
+import javax.swing.JFrame;
+
 public class Main
 {
-	
+	public static void main(String args[])
+	{
+		// The window for the program
+		JFrame frame = new JFrame("JME - JPEG Metadata Extractor");
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setSize(600, 600);
+
+		System.out.println("Opening frame");
+		frame.setVisible(true);
+	}
 }
