096d4f2f471b8978d15a4a8062c230e6f8ace17a
Nick Mirasol <takutastrong@yahoo.com>
2019-09-28 10:59:40 -0700

    Moved geo server from main to ref file

--- src/main/java/mirasoln/jme/JmeRef.java
+++ src/main/java/mirasoln/jme/JmeRef.java
@@ -2,8 +2,11 @@ package mirasoln.jme;
 
 public class JmeRef
 {
+	public static final String MAP_API = "https://maps.googleapis.com/maps/api/geocode/json?";
 	public static final String API_KEY = "";
 
 	public static final String JPG_TAG_LAT = "[GPS] GPS Latitude - ";
 	public static final String JPG_TAG_LNG = "[GPS] GPS Longitude - ";
+	
+	public static final String[] VALID_JPEG_EXT = { "jpg", "jpeg", "jpe", "jif", "jfif", "jfi"};
 }
