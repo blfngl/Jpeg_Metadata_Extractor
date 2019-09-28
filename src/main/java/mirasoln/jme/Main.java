56af576da33bb869b0c7d0883297ee2cec1a00c8
Nick Mirasol <takutastrong@yahoo.com>
2019-09-28 12:06:34 -0700

    Fixed typo

--- src/main/java/mirasoln/jme/Main.java
+++ src/main/java/mirasoln/jme/Main.java
@@ -58,7 +58,7 @@ public class Main
 			for (String image : args)
 				processImage(image);
 
-			System.out.println("\nPress any key to exit.");
+			System.out.println("\nPress enter to exit.");
 			Scanner scan = new Scanner(System.in);
 			scan.nextLine();
 		}
