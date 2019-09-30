0262117b6ff019f2a1c322d713cdc4b2864e6311
Nick Mirasol <takutastrong@yahoo.com>
2019-09-30 12:24:13 -0700

    Method reorganization

--- src/main/java/mirasoln/jme/Main.java
+++ src/main/java/mirasoln/jme/Main.java
@@ -118,100 +118,6 @@ public class Main
 	}
 
 	/**
-	 * Runs through all arguments and determines if flags should be turned on. Returns a list
-	 * of the arguments provided by the user minus all commands that may have been included.
-	 * @param args The arguments provided by the user
-	 * @return The arguments provided by the user minus all commands
-	 */
-	private static ArrayList<String> processAndRemoveFlags(String[] args)
-	{
-		ArrayList<String> argList = new ArrayList<String>();
-
-		for (String arg : args)
-		{
-			// Check if argument is a command
-			if (arg.substring(0, 1).equals("-"))
-			{
-				if (arg.equals(JmeRef.FLAG_BONUS_INFO))
-				{
-					flagBonusInfo = true;
-					logger.info("Displaying bonus info.");
-				}
-
-				else if (arg.equals(JmeRef.FLAG_GENERATE_LINK))
-				{
-					flagGenerateLink = true;
-					logger.info("Generating google maps links of locations found.");
-				}
-
-				else if (arg.equals(JmeRef.FLAG_ALL_FILES))
-					flagAllFiles = true;
-
-				else
-					logger.log(Level.WARNING, arg + " is an invalid command!");
-			}
-
-			// Otherwise add the argument to the command-free list
-			else
-				argList.add(arg);
-		}
-
-		return argList;
-	}
-
-	/**
-	 * Processes all files within the working directory of the jar.
-	 */
-	private static void processDirectory()
-	{
-		File folder = new File(System.getProperty("user.dir"));
-
-		logger.info("Processing all files in working directory:");
-		logger.info(folder.getAbsolutePath() + "\n");
-
-		for (File file : folder.listFiles())
-			processFile(file.getName());
-	}
-
-	/**
-	 * Initializes logging
-	 */
-	private static void initLogger()
-	{
-		String currentDate = new Date(System.currentTimeMillis()).toString();
-		currentDate = currentDate.replace(":", "-");
-
-		logger = Logger.getLogger("JME " + currentDate);
-
-		try {
-			Path path = FileSystems.getDefault().getPath(JmeRef.DIR_LOGS);
-
-			if (!Files.exists(path))
-			{
-				logger.log(Level.WARNING, "Log output directory not found. Creating...");
-				Files.createDirectories(Paths.get(JmeRef.DIR_LOGS));
-			}
-
-			FileHandler fh = new FileHandler(JmeRef.DIR_LOGS + "/" + currentDate + ".log");
-			fh.setFormatter(new JmeLoggerFormatter());
-			logger.addHandler(fh);
-			logger.setUseParentHandlers(false);
-		}
-
-		catch (SecurityException e) {
-			logger.log(Level.SEVERE, e.getMessage());
-			logger.info(e.getStackTrace().toString());
-			e.printStackTrace();
-		}
-
-		catch (IOException e) {
-			logger.log(Level.SEVERE, e.getMessage());
-			logger.info(e.getStackTrace().toString());
-			e.printStackTrace();
-		}
-	}
-
-	/**
 	 * Processes a file, first confirming it is a jpeg and then looking for location metadata.
 	 * @param filePath The file to process
 	 */
@@ -261,21 +167,90 @@ public class Main
 	}
 
 	/**
-	 * Returns true if a file has a valid jpeg file extension.
-	 * @param filePath The file to check
-	 * @return True if the file has a valid jpeg extension
+	 * This method runs through all the tags found in the metadata of a file and
+	 * looks for the GPS information attached, if any.
+	 * @param filePath the image
+	 * @return the coordinates
+	 * @throws IOException 
+	 * @throws ImageProcessingException 
 	 */
-	private static boolean confirmJpeg(String filePath)
+	private static String getCoordsFromImage(String filePath) throws ImageProcessingException, IOException
 	{
-		String fileName = new File(filePath).getName();
-		int indexExtension = fileName.lastIndexOf(".");
-		String fileExtension = fileName.substring(indexExtension + 1);
+		StringBuilder coords = new StringBuilder();
 
-		for (String extension : JmeRef.VALID_JPEG_EXT)
-			if (extension.equals(fileExtension))
-				return true;
+		File file = new File(filePath);
+		Metadata meta = ImageMetadataReader.readMetadata(file);
 
-		return false;
+		for (Directory dir : meta.getDirectories())
+		{
+			for (Tag tag : dir.getTags())
+			{
+				String theTag = tag.toString();
+
+				if (theTag.contains(JmeRef.JPG_TAG_LAT))
+				{
+					theTag = theTag.substring(JmeRef.JPG_TAG_LAT.length());
+					theTag = convertDMStoDecimal(theTag);
+					coords.append(theTag);
+					coords.append(",");
+				}
+
+				else if (theTag.contains(JmeRef.JPG_TAG_LNG))
+				{
+					theTag = theTag.substring(JmeRef.JPG_TAG_LNG.length());
+					theTag = convertDMStoDecimal(theTag);
+					coords.append(theTag);
+				}
+			}
+		}
+
+		return coords.toString();
+	}
+
+	/**
+	 * Gets a json file of information about a location specified by lat and long coords.
+	 * @param coords the lat and long coords, separated by a ','
+	 * @return the json containing information about the location
+	 * @throws ParseException
+	 */
+	private static JSONObject getLocationData(String coords) throws ParseException
+	{
+		// Build the url
+		String webAddress = buildUrl(coords);
+
+		try
+		{
+			// Make request
+			URL url = new URL(webAddress);
+			// TCP get request
+			InputStream stream = url.openStream();
+
+			try
+			{
+				// Turn the result into a json
+				BufferedReader reader = new BufferedReader(new InputStreamReader(url.openStream()));
+				StringBuffer buffer = new StringBuffer();
+				int read;
+				char[] chars = new char[1024];
+
+				while ((read = reader.read(chars)) != -1)
+					buffer.append(chars, 0, read);
+
+				JSONParser parser = new JSONParser();
+				JSONObject json = (JSONObject) parser.parse(buffer.toString());
+
+				return json;
+			}
+
+			// Don't cross the streams
+			finally {
+				stream.close();
+			}
+		} catch (IOException e) {
+			logger.log(Level.SEVERE, e.getMessage());
+			logger.log(Level.SEVERE, e.getStackTrace().toString());
+			throw new RuntimeException(e);
+		}
 	}
 
 	/**
@@ -336,49 +311,21 @@ public class Main
 	}
 
 	/**
-	 * Gets a json file of information about a location specified by lat and long coords.
-	 * @param coords the lat and long coords, separated by a ','
-	 * @return the json containing information about the location
-	 * @throws ParseException
+	 * Returns true if a file has a valid jpeg file extension.
+	 * @param filePath The file to check
+	 * @return True if the file has a valid jpeg extension
 	 */
-	private static JSONObject getLocationData(String coords) throws ParseException
+	private static boolean confirmJpeg(String filePath)
 	{
-		// Build the url
-		String webAddress = buildUrl(coords);
+		String fileName = new File(filePath).getName();
+		int indexExtension = fileName.lastIndexOf(".");
+		String fileExtension = fileName.substring(indexExtension + 1);
 
-		try
-		{
-			// Make request
-			URL url = new URL(webAddress);
-			// TCP get request
-			InputStream stream = url.openStream();
+		for (String extension : JmeRef.VALID_JPEG_EXT)
+			if (extension.equals(fileExtension))
+				return true;
 
-			try
-			{
-				// Turn the result into a json
-				BufferedReader reader = new BufferedReader(new InputStreamReader(url.openStream()));
-				StringBuffer buffer = new StringBuffer();
-				int read;
-				char[] chars = new char[1024];
-
-				while ((read = reader.read(chars)) != -1)
-					buffer.append(chars, 0, read);
-
-				JSONParser parser = new JSONParser();
-				JSONObject json = (JSONObject) parser.parse(buffer.toString());
-
-				return json;
-			}
-
-			// Don't cross the streams
-			finally {
-				stream.close();
-			}
-		} catch (IOException e) {
-			logger.log(Level.SEVERE, e.getMessage());
-			logger.log(Level.SEVERE, e.getStackTrace().toString());
-			throw new RuntimeException(e);
-		}
+		return false;
 	}
 
 	/**
@@ -400,47 +347,6 @@ public class Main
 		b.append("&key=" + API_KEY);
 
 		return b.toString();
-	}
-
-	/**
-	 * This method runs through all the tags found in the metadata of a file and
-	 * looks for the GPS information attached, if any.
-	 * @param filePath the image
-	 * @return the coordinates
-	 * @throws IOException 
-	 * @throws ImageProcessingException 
-	 */
-	private static String getCoordsFromImage(String filePath) throws ImageProcessingException, IOException
-	{
-		StringBuilder coords = new StringBuilder();
-
-		File file = new File(filePath);
-		Metadata meta = ImageMetadataReader.readMetadata(file);
-
-		for (Directory dir : meta.getDirectories())
-		{
-			for (Tag tag : dir.getTags())
-			{
-				String theTag = tag.toString();
-
-				if (theTag.contains(JmeRef.JPG_TAG_LAT))
-				{
-					theTag = theTag.substring(JmeRef.JPG_TAG_LAT.length());
-					theTag = convertDMStoDecimal(theTag);
-					coords.append(theTag);
-					coords.append(",");
-				}
-
-				else if (theTag.contains(JmeRef.JPG_TAG_LNG))
-				{
-					theTag = theTag.substring(JmeRef.JPG_TAG_LNG.length());
-					theTag = convertDMStoDecimal(theTag);
-					coords.append(theTag);
-				}
-			}
-		}
-
-		return coords.toString();
 	}
 
 	/**
@@ -469,6 +375,48 @@ public class Main
 	}
 
 	/**
+	 * Runs through all arguments and determines if flags should be turned on. Returns a list
+	 * of the arguments provided by the user minus all commands that may have been included.
+	 * @param args The arguments provided by the user
+	 * @return The arguments provided by the user minus all commands
+	 */
+	private static ArrayList<String> processAndRemoveFlags(String[] args)
+	{
+		ArrayList<String> argList = new ArrayList<String>();
+
+		for (String arg : args)
+		{
+			// Check if argument is a command
+			if (arg.substring(0, 1).equals("-"))
+			{
+				if (arg.equals(JmeRef.FLAG_BONUS_INFO))
+				{
+					flagBonusInfo = true;
+					logger.info("Displaying bonus info.");
+				}
+
+				else if (arg.equals(JmeRef.FLAG_GENERATE_LINK))
+				{
+					flagGenerateLink = true;
+					logger.info("Generating google maps links of locations found.");
+				}
+
+				else if (arg.equals(JmeRef.FLAG_ALL_FILES))
+					flagAllFiles = true;
+
+				else
+					logger.log(Level.WARNING, arg + " is an invalid command!");
+			}
+
+			// Otherwise add the argument to the command-free list
+			else
+				argList.add(arg);
+		}
+
+		return argList;
+	}
+
+	/**
 	 * Reads metadata information from a given jpeg.
 	 * @throws ImageProcessingException
 	 * @throws IOException
@@ -483,6 +431,20 @@ public class Main
 		for (Directory dir : meta.getDirectories())
 			for (Tag tag : dir.getTags())
 				logger.info(tag.toString());
+	}
+
+	/**
+	 * Processes all files within the working directory of the jar.
+	 */
+	private static void processDirectory()
+	{
+		File folder = new File(System.getProperty("user.dir"));
+
+		logger.info("Processing all files in working directory:");
+		logger.info(folder.getAbsolutePath() + "\n");
+
+		for (File file : folder.listFiles())
+			processFile(file.getName());
 	}
 
 	/**
@@ -540,5 +502,43 @@ public class Main
 
 		panelZip.add(labelPanelZip);
 		frame.getContentPane().add(BorderLayout.SOUTH, panelZip);
+	}
+
+	/**
+	 * Initializes logging
+	 */
+	private static void initLogger()
+	{
+		String currentDate = new Date(System.currentTimeMillis()).toString();
+		currentDate = currentDate.replace(":", "-");
+
+		logger = Logger.getLogger("JME " + currentDate);
+
+		try {
+			Path path = FileSystems.getDefault().getPath(JmeRef.DIR_LOGS);
+
+			if (!Files.exists(path))
+			{
+				logger.log(Level.WARNING, "Log output directory not found. Creating...");
+				Files.createDirectories(Paths.get(JmeRef.DIR_LOGS));
+			}
+
+			FileHandler fh = new FileHandler(JmeRef.DIR_LOGS + "/" + currentDate + ".log");
+			fh.setFormatter(new JmeLoggerFormatter());
+			logger.addHandler(fh);
+			logger.setUseParentHandlers(false);
+		}
+
+		catch (SecurityException e) {
+			logger.log(Level.SEVERE, e.getMessage());
+			logger.info(e.getStackTrace().toString());
+			e.printStackTrace();
+		}
+
+		catch (IOException e) {
+			logger.log(Level.SEVERE, e.getMessage());
+			logger.info(e.getStackTrace().toString());
+			e.printStackTrace();
+		}
 	}
 }
